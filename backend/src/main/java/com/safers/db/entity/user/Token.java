f5a6db543704f34f11af7b024cbcb8c846adff2d
eona1301 <eona1301@gmail.com>
2021-10-27 20:33:29 +0900

    [#5] feat: create token entity

--- backend/src/main/java/com/safers/db/entity/user/Token.java
+++ backend/src/main/java/com/safers/db/entity/user/Token.java
@@ -1,18 +1,18 @@
-package com.safers.db.entity;
+package com.safers.db.entity.user;
 
+import com.safers.db.entity.BaseEntity;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
-@Table(name="token", schema = "safer")
+@Table(name="token", schema = "safers")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Token extends BaseEntity{
+public class Token extends BaseEntity {
 
     @OneToOne
     @JoinColumn(name = "id")
