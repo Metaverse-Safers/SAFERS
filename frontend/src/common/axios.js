f14d25de187a5b1f73692c15f5ab5d677ba4edcc
zxd9857@gmail.com <zxd9857@gmail.com>
2021-10-29 04:59:40 +0900

    [#3] feat: error handler + authentication process

--- frontend/src/common/axios.js
+++ frontend/src/common/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import store from '@/store';
+import router from '@/router';
 
 const http = axios.create({
   baseURL: "/api",
@@ -13,8 +14,7 @@ http.interceptors.request.use(
   config => {
     const isAuthenticated = store.getters["user/isAuthenticated"];
     if(isAuthenticated){
-      console.log(store.getters["user/token"].accessToken);
-      config.headers.common["Authorization"] = store.getters["user/token"].accessToken;
+      config.headers.common["authorization"] = store.getters["user/token"].accessToken;
     }
     return config;
   },
@@ -24,11 +24,27 @@ http.interceptors.request.use(
 )
 
 // 로그인이 정상적으로 되어있지 않다면
-http.interceptors.response.use((response) => response, (error) => {
+http.interceptors.response.use((response) => response, async (error) => {
   console.log(error.response.data);
 
-  if(error.response.data.status == 400){
-    alert("로그인이 필요합니다!");
+  if(error.response.data.status == 401){
+    const isAuthenticated = store.getters["user/isAuthenticated"]; 
+
+    if(!isAuthenticated){
+      alert("로그인이 필요합니다!");
+      router.push({ name: 'login' });
+    }
+    else{
+      // accessToken 만료 시, 다시 토큰 발급받아서 재요청
+      const originalRequest = error.config;
+      const refreshToken = store.getters["user/token"].refreshToken;
+      await store.dispatch("user/refreshToken", { "refreshToken" : refreshToken});
+
+      return axios(originalRequest)
+    }
+  }
+  else if(error.response.data.status == 500){
+    alert("서버에 오류가 발생하였습니다. 다시 시도해주세요.");
   }
 
   throw error;
