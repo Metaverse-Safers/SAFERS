3556a4802d6ea912130115e9732040bcf755ad80
이영주 <clleo97@naver.com>
2021-10-28 11:46:59 +0900

    [#7] feat: update axios url from http to https

--- frontend/src/common/axios.js
+++ frontend/src/common/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://localhost:8080/api",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json; charset = utf-8",
