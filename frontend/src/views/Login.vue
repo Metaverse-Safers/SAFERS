b6138b449f25d6a5d8d4a33487cf868ceeb95d7d
zxd9857@gmail.com <zxd9857@gmail.com>
2021-10-28 10:20:20 +0900

    [#3] fix: solved kakao init problem

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -13,9 +13,9 @@
 export default{
     methods: {
         kakaoLogin(){
-            Kakao.Auth.authorize({
-            redirectUri: "http://localhost:8081/login/callback" // 8081: frontend, 8080: backend로 두고 테스트함.
-            //redirectUri: "https://k5a403.p.ssafy.io/login/callback" //배포용
+            this.$kakao.Auth.authorize({
+            // redirectUri: "http://localhost:8081/login/callback" // 8081: frontend, 8080: backend로 두고 테스트함.
+            redirectUri: "https://k5a403.p.ssafy.io/login/callback" //배포용
             });
         },
     },
