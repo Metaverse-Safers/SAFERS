623a5ed1d46410f04df98bdedfcc620424aff342
zxd9857@gmail.com <zxd9857@gmail.com>
2021-11-01 16:11:41 +0900

    [#9] chore: change code to post server
    Former-commit-id: 24a89440fbda55e8ec4c361e94701029fa8a7c27

--- backend/src/main/java/com/safers/db/repository/CodeRepository.java
+++ backend/src/main/java/com/safers/db/repository/CodeRepository.java
@@ -4,8 +4,10 @@ import com.safers.db.entity.code.Code;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.OptionalInt;
+
 
 @Repository
 public interface CodeRepository extends JpaRepository<Code, String> {
-
 }
