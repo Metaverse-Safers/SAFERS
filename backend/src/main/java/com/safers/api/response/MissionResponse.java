e9a7ceb4f2f205168f525983c69bfb99f46dd09d
이영주 <clleo97@naver.com>
2021-11-09 23:06:24 +0900

    [#21] feat: add mission complete API
    Former-commit-id: f53b096f6d9c830f5d321647d3483c129eaf4853

---
+++ backend/src/main/java/com/safers/api/response/MissionResponse.java
@@ -0,0 +1,26 @@
+package com.safers.api.response;
+
+import com.safers.db.entity.unity.Mission;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class MissionResponse {
+
+    String id;
+    String title;
+    String nextMission;
+    String resultId;
+
+    public static MissionResponse of(Mission mission) {
+        return MissionResponse.builder()
+                .id(mission.getId())
+                .title(mission.getTitle())
+                .nextMission(mission.getNextMission())
+                .resultId(mission.getResultId())
+                .build();
+    }
+}
