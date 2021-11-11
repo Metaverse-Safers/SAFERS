ad124ba770886bd5dc046390600e9337bb4d6b8d
안상훈 <ahnsang9@naver.com>
2021-11-11 17:00:42 +0900

    [#2] chore
    Former-commit-id: c40f3904773627c33e36f10c48f6ec2aff20170a

--- frontend/src/components/Board/BoardDetail.vue
+++ frontend/src/components/Board/BoardDetail.vue
@@ -1,30 +1,77 @@
 <template>
-    <div class="main">
-        <div class='box'>
-            <div id="img">
-                <img :src="imageInfo.urls.small" style="height:80vh" v-if='Hw'/>
-                <img :src="imageInfo.urls.small" style="width:46.6vw; vertical-align: middle;" v-else/>
+    <div class="detail-main">
+        <div class='detail-box'>
+            <div class="detail-img">
+                <img :src="info.fileList[0].filePath" style="width: 100%; height:100%; object-fit: contain;" @click="a"/>
+            </div>                                 
+            <div class="detail-user">
+                <img :src="info.profileUrl"/>
+                <div>{{info.nickName}}</div>
             </div>
-            <div id="text">
-                <p>{{imageInfo.created_at}}</p>
+            <div class="detail-text">
+                <p class="detail-title">{{info.title}}</p>
+                <p class="detail-content">{{info.content}}</p>
+                <div class="detail-comment-list" v-for="(list, idx) in commentList" v-bind:key="idx">
+                    <img :src="list.profileUrl"/>
+                    <div>{{data.nickName}}</div>
+                    <br>
+                    <p class="detail-content">{{data.content}}</p>
+                </div>
+                <div class="detail-comment-register">
+                    <input class="detail-comment-write" placeholder="댓글 달기..." v-model="comment" required/>
+                    <p class="detail-comment-register-button" @click="commentRegister">등록</p>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+    import { mapGetters } from "vuex";
     export default {
         data() {
             return{
-                imageInfo:"",
-                Hw: false
+                comment: "",
+                commentList: {}
             }
         },
-        created() {
-            this.imageInfo = this.$route.query.image;
-            if (this.imageInfo.height > this.imageInfo.width){
-                this.Hw = true
+        props: {
+            info: []
+        },
+        computed: {
+            ...mapGetters({
+                userProfile: "user/userProfile"
+            })
+        },
+        methods:{
+            commentGet() {
+                axios
+                .get('/api/board/comment/' + this.info.id)
+                .then(res => {
+                    this.commentList = res.data
+                    console.log(this.commentList)
+                })
+            },
+            commentRegister() {
+                const uploadComment = new FormData();
+                uploadComment.append("board_id", this.info.id);
+                uploadComment.append("comment", this.comment);
+                uploadComment.append("userId", this.userProfile.id);
+                axios
+                .post('/api/board/comment/' + this.info.id, uploadComment)
+                .then(res => { // eslint-disable-line no-unused-vars
+                    console.log("성공")
+                    this.commentGet();
+                })
+                
+            },
+            a(){
+                console.log(this.info)
             }
+        },
+        mounted(){
+            
         }
     }
 </script>
@@ -42,9 +89,10 @@
         display: grid;
         grid-template-columns: 2fr 1fr;
     }
-    #img{
-        width: 100%;
-        height: 100%;
+    .detail-box *{
+        font-family: "IBMPlexSansKR-SemiBold";
+    }
+    .detail-img{
         text-align: center;
         line-height: 80vh;
     }
@@ -58,4 +106,34 @@
         border-left: 1px rgb(219, 219, 219) solid;
         border-right: 1px rgb(219, 219, 219) solid;
     }
+    .detail-title {
+        height: auto;
+        padding: 10px;
+        font-size: 20px;
+        margin-bottom: 0 !important;
+    }
+    .detail-content {
+        height: auto;
+        border-top: 1px rgb(230, 230, 230) solid;
+        padding: 10px;
+        margin-bottom: 0 !important;
+    }
+    .detail-comment-register {
+        display: flex;
+        align-items: center;
+        padding: 2px;
+        border-top: 1px rgb(230, 230, 230) solid;
+    }
+    .detail-comment-write {
+        border: none;
+        width: 100%;
+        font-size: 1.5vh;
+    }
+    .detail-comment-write:focus {
+        outline: none;
+    }
+    .detail-comment-register-button {
+        margin-bottom: 0 !important;
+    }
+    
 </style>
---
+++ frontend/src/components/Board/BoardList.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="list-wrap">
+    <div class="list-nav" v-show="boardDetail" >
+      <i class="fas fa-arrow-left fa-2x" @click="boardDetailFunc"></i>
+      <h2 class="imb-font-semi-bold">'{{info.nickName}}' 님의 게시물</h2>
+    </div>
+    <div class="masonry" v-show="!boardDetail">
+      <div class="mItem" v-for="(data, idx) in boardList" v-bind:key="idx" >
+        <img :src= data.fileList[0].filePath @click="boardDetailInfo(data)">
+      </div>
+      <infinite-loading @infinite='infiniteHandler'>
+        <div 
+          slot='no-more'
+          style="color:rgb(102,102,102); font-size: 14px; padding:10px 0px;">
+          목록의 끝입니다
+        </div>
+      </infinite-loading>
+    </div>
+    <div class="board-detail" v-if="boardDetail">
+      <BoardDetail :info="info"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+  import InfiniteLoading from 'vue-infinite-loading';
+  import BoardDetail from "./BoardDetail.vue";
+  export default {
+    components: {
+      InfiniteLoading,
+      BoardDetail,
+    },
+    data(){
+      return{
+        info: [],
+        boardDetail: false,
+        boardList: [],
+        page: 0
+      }
+    },
+    methods:{
+      infiniteHandler($state){
+        setTimeout(1000);
+        axios
+          .get('/api/board/list/' + this.page)
+          .then(res => {
+            if(res.data.length == 0){
+              $state.complete();
+            }
+            else{
+              setTimeout(() => {
+                for(let i=0; i<res.data.length; i++)
+                {
+                  if(res.data[i].fileList[0].filePath)
+                    this.boardList.push(res.data[i]);
+                }
+                this.page++;
+                $state.loaded();
+              },700)
+              
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      },
+      boardDetailInfo(data){
+        this.info=data;
+        this.boardDetail=true;
+        this.$emit('detail',true);
+      },
+      boardDetailFunc(){
+        this.boardDetail=false;
+        this.$emit('detail',false);
+      }
+    }
+  }
+</script>
+
+<style>
+.list-wrap{
+  height: 100%;
+}
+
+.list-nav {
+  display: grid;
+  grid-template-columns: 1fr 10fr 1fr;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.list-nav > h2 {
+  grid-column: 2/3;
+  justify-self: center;
+  margin-bottom: 0 !important;
+}
+
+.list-nav > i {
+  grid-column: 1/2;
+  justify-self: start;
+}
+
+.list-nav > i:hover{
+  filter: brightness(5%);
+  cursor: pointer;
+}
+
+.mItem > img {
+  width: 100%;
+  border-radius: 10px;
+}
+
+.mItem > img:hover {
+  filter: brightness(70%);
+  cursor: pointer;
+}
+
+.masonry {
+	column-count: 6;
+	column-gap: 16px;
+  padding-top: 10px;
+  border-top: 1px rgb(220, 220, 220) solid;
+}
+
+.masonry .mItem {
+  display: inline-block;
+  margin-bottom: 16px;
+  width: 100%;
+}
+
+.board-detail{
+  height: 85%;
+}
+
+@media (max-width: 1600px) {
+  .masonry {
+    column-count: 5;
+  }
+}
+
+@media (max-width: 1200px) {
+  .masonry {
+    column-count: 4;
+  }
+}
+
+@media (max-width: 800px) {
+  .masonry {
+    column-count: 3;
+  }
+}
+
+@media (max-width: 400px) {
+  .masonry {
+    column-count: 2;
+  }
+}
+
+
+</style>
