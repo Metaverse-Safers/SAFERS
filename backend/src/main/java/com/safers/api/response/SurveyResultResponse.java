91b425d9ef3b182d3e2e103a9ab175bea393d1ad
eona1301 <eona1301@gmail.com>
2021-11-12 13:58:50 +0900

    [#22] feat: create survey result response update
    Former-commit-id: f40f54f0f39f2319223e54f3b0f7ac9c17e37fcf

---
+++ backend/src/main/java/com/safers/api/response/SurveyResultResponse.java
@@ -0,0 +1,54 @@
+package com.safers.api.response;
+
+import com.safers.api.service.SurveyService;
+import com.safers.db.entity.Survey;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+
+@Getter
+@Setter
+@Builder
+public class SurveyResultResponse extends BaseResponse{
+    @ApiModelProperty
+    String mbti;
+
+    @ApiModelProperty
+    String area;
+
+    @ApiModelProperty
+    String name;
+
+    @ApiModelProperty
+    String imageUrl;
+
+    @ApiModelProperty
+    String introduce;
+
+    @ApiModelProperty
+    List<String> explanation;
+
+    @ApiModelProperty
+    List<String> extinction;
+
+    @ApiModelProperty
+    List<String> personality;
+
+    @ApiModelProperty
+    List<SurveySimpleResponse> buddy;
+
+    @ApiModelProperty
+    List<SurveySimpleResponse> enemy;
+
+    public static SurveyResultResponse of(Integer statusCode, String message, SurveyResultResponse survey){
+        survey.setStatusCode(statusCode);
+        survey.setStatusMessage(message);
+        return survey;
+    }
+
+
+}
