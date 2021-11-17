29208036cc9222fcbaf2579160cce5084586b236
zxd9857@gmail.com <zxd9857@gmail.com>
2021-11-18 01:08:53 +0900

    FIX URL ACCESS
    Former-commit-id: 774328745634f26c59fc500038aec66a2d346701
    Former-commit-id: 034262d0085141b03edef04733cf4e0fea8d43b3

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,10 +16,10 @@ const route = new VueRouter({
     { path: '/login/callback', name: 'kakaoCallback', component: () => import("@/components/User/KakaoCallback.vue") },
 
     // register 페이지
-    { path: '/register', name: 'register', component: () => import("@/views/Register.vue") },
+    { path: '/register', name: 'register', meta: {requireAuth:true}, component: () => import("@/views/Register.vue") },
 
     // world 페이지
-    { path: '/world', name: 'world', component: () => import("@/views/World.vue") },
+    { path: '/world', name: 'world', meta: {requireAuth:true}, component: () => import("@/views/World.vue") },
 
     // information 페이지
     { path: '/info', name: 'info', component: () => import("@/views/Info.vue") }
@@ -27,7 +27,16 @@ const route = new VueRouter({
   ]
 })
 
-route.afterEach((to) => {
+
+// URL이 변경되기 전, 호출되는 함수
+route.beforeEach((to, from, next) => {
+  // 로그인이 필요한 페이지에 들어가기전, 로그인 여부 확인
+  if(to.matched.some(record => record.meta.requireAuth)){
+    if(!store.getters["user/isAuthenticated"]){ 
+      route.push("/login");
+    }
+  }
+
   // 월드에서 나온 경우
   if(store.getters["user/path"] == "/world"){
     store.commit("user/SET_IS_DESTROYED", true);
@@ -36,6 +45,7 @@ route.afterEach((to) => {
   }
   // 이동한 PATH 저장
   store.commit("user/SET_PATH", to.path);
+  next();
 })
 
 export default route;
